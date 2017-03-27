b7221b9123b2f853db37013b3415280ab780925b
Amd-PC\Amd <caiodallecio@gmail.com>
2017-03-27 17:27:02 -0300

    Implemented Operands, reaching a leaf at p_Literal

--- parser.py
+++ parser.py
@@ -56,6 +56,7 @@ class Parser():
         """ Identifier : ID """
         p[0] = p[1]
 
+    #Simplified
     def p_Mode(self,p):
         """ Mode :  ModeName """
 
@@ -64,7 +65,68 @@ class Parser():
     def p_Initialization(self,p):
         """ Initialization : ATRIB Expression """
         p[0] = Initialization(p[2])
-        
+
+    #Simplified
+    def p_Expression(self,p):
+        """ Expression : Operand0"""
+        p[0] = Expression(p[1])
+
+    def p_Operand0(self,p):
+        """" Operand0 : Operand1
+                      | Operand0 Operator1 Operand1 """
+        if len(p) == 2 :
+            p[0] = Operand0(p[1],False,False)
+        else:
+            p[0] = Operand0(p[1],p[2],p[3])
+
+    def p_Operand1(self,p):
+        """ Operand1 : Operand2
+                     | Operand1 Operator2 Operand2 """
+        if len(p) == 2 :
+            p[0] = Operand1(p[1],False,False)
+        else:
+            p[0] = Operand1(p[1],p[2],p[3])
+
+    def p_Operand2(self,p):
+        """ Operand2 : Operand3
+                     | Operand2 MUL Operand3
+                     | Operand2 DIV Operand3
+                     | Operand2 MOD Operand3 """
+        if len(p) == 2:
+            p[0] = Operand2(p[1],False,False)
+        else:
+            p[0] = Operand2(p[1],p[2],p[3])
+
+    def p_Operand3(self,p):
+        """ Operand3 : MINUS Operand4
+                     | NOT  Operand4
+                     | ICONST """
+        if len(p) == 2:
+            p[0] = Operand3(p[1],False)
+        else:
+            p[0] = Operand3(p[1],p[2])
+
+    #Simplified
+    def p_Operand4(self,p):
+        """ Operand4 =  PrimitiveValue """
+        p[0] = Operand4(p[1])
+
+    def p_PrimitiveValue(self,p):
+        """ PrimitiveValue = Literal
+                           | ValueArrayElement
+                           | ValueArraySlice
+                           | ParenthesizedExpression """
+        p[0] = PrimitiveValue(p[1])
+
+    def p_Literal(self,p):
+        """" Literal : ICONST
+                     | FALSE
+                     | TRUE
+                     | CHALIT
+                     | NULL
+                     | STR """
+        p[0] = Literal(p[1])
+
     def p_error(self, p):
             print("Systax error in input()")
 
